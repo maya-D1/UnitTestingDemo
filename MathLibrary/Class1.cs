1fb942b19e9ae4fee57a3e84f110c00a86f5f754
maya-D1 <maya.1dakkak@gmail.com>
2025-06-15 00:07:55 +0300

    before editing

---
+++ MathLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace MathLibrary;
+
+/// <summary> 
+/// ةطيسب ةيضاير لاود ىلع يوتحي. 
+/// </summary> 
+public static class Calculator 
+{ 
+    /// <summary> 
+    /// نيحيحص نيددع برض لصاح بسحت. 
+    /// </summary> 
+    /// <param name="a">لولأا ددعلا</param> 
+    /// <param name="b">يناثلا ددعلا</param> 
+    /// <returns>برضلا جتان</returns> 
+    public static int Multiply(int a, int b) 
+    { 
+        return a * b; 
+    } 
+} 
